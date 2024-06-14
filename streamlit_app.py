9099ecd2c0fc28db6b32abb6b479e86e9e766c58
dheeraj hegde <dheeraj.r.hegde@gmail.com>
2024-06-14 13:59:51 -0400

    Initial Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,6 +76,7 @@ else:
                    f"Bearer {token}"
                }
     url = f"https://fhir.cigna.com/PatientAccess/v1-devportal/Patient?_id={user_id}"
+    print(url)
     jsonString = requests.get(url, headers=headers)
     data = json.loads(jsonString.content)
     #st.write(data)
