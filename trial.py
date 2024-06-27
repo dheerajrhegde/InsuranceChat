c0073af78673ebd0502af7c7ac72121b663c665f
dheeraj.r.hegde@gmail.com <dheeraj.r.hegde@gmail.com>
2024-06-27 12:54:03 -0400

    changed auth parameters

--- trial.py
+++ trial.py
@@ -32,7 +32,7 @@ body = {
     "client_secret":CLIENT_SECRET
 }
 st.write("body to post is ...",body)
-response = requests.post(token_url, json=body)
+response = requests.post(token_url, data=json.dumps(body))
 st.write(response.content)
 
 
