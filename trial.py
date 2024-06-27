275886caa8073aef97f7aa85c09ff981b5a87da9
dheeraj.r.hegde@gmail.com <dheeraj.r.hegde@gmail.com>
2024-06-27 09:18:31 -0400

    changed auth parameters

--- trial.py
+++ trial.py
@@ -18,5 +18,5 @@ st.write(oauth2client)
 az_request = oauth2client.authorization_request(scope="openid fhirUser patient/*.read")
 st.write(az_request.uri)
 
-response = requests.post(az_request.url, data=az_request.body, headers=az_request.headers)
+response = requests.post(az_request.url)
 st.write(response)
