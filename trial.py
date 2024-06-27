6e7d5232816b6bdee32e9e59c7028f6ae916d752
dheeraj.r.hegde@gmail.com <dheeraj.r.hegde@gmail.com>
2024-06-27 09:44:51 -0400

    changed auth parameters

--- trial.py
+++ trial.py
@@ -22,44 +22,15 @@ st.write(az_request.uri)
 st.write(st.session_state)
 st.write(st.query_params)
 
-
-AUTHORIZE_ENDPOINT = "https://r-hi2.cigna.com/mga/sps/oauth/oauth20/authorize"
-TOKEN_ENDPOINT = "https://r-hi2.cigna.com/mga/sps/oauth/oauth20/token"
-REVOKE_ENDPOINT = "https://r-hi2.cigna.com/mga/sps/oauth/oauth20/revoke"
-oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_ENDPOINT, TOKEN_ENDPOINT, REVOKE_ENDPOINT)
-REDIRECT_URI = "https://trialpy.streamlit.app/"
-SCOPE = "openid fhirUser patient/*.read"
-
-result = oauth2.authorize_button(
-        name="Continue with Cigna",
-        icon="https://www.google.com.tw/favicon.ico",
-        redirect_uri=REDIRECT_URI,
-        scope=SCOPE,
-        key="cigna",
-        extras_params={"prompt": "consent", "access_type": "offline"},
-        use_container_width=True,
-        pkce='S256',
-    )
-
-st.write(result.get('token'))
-
-token = result.get("token")["access_token"]
-headers = {"Authorization":
-               f"Bearer {token}"
-           }
-
-# Get user identifier
-url = "https://fhir.cigna.com/PatientAccess/v1-devportal/$userinfo"
-jsonString = requests.get(url, headers=headers)
-data = json.loads(jsonString.content)
-st.write(token)
-st.write(data)
-user_id = data["parameter"][0]["valueString"]
+token_url = "https://hi2.cigna.com/mga/sps/oauth/oauth20/token"
+body = {
+    "grant_type": "authorization_code",
+    "code": "code-received-in-redirect-url",
+    "redirect_uri": "https://yourhealthapp.com://oauth",
+    "client_id":CLIENT_ID,
+    "client_secret":CLIENT_SECRET
+}
+response = requests.post(token_url, data=body)
+st.write(response.json())
 
 
-url = f"https://fhir.cigna.com/PatientAccess/v1-devportal/Patient?_id={user_id}"
-#st.write(url)
-jsonString = requests.get(url, headers=headers)
-st.write(jsonString.headers)
-data = json.loads(jsonString.content)
-st.write(data)
