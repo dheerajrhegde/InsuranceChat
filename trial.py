c292c4bd7970d12a00bda4bf80012611d396a097
dheeraj.r.hegde@gmail.com <dheeraj.r.hegde@gmail.com>
2024-06-27 12:51:43 -0400

    changed auth parameters

--- trial.py
+++ trial.py
@@ -29,10 +29,10 @@ body = {
     "code": st.query_params["code"],
     "redirect_uri": "https://trialpy.streamlit.app/",
     "client_id":CLIENT_ID,
-    "client_secret":CLIENT_SECRET
+    "client_secret":CLIENT_SECRET,
 }
 st.write("body to post is ...",body)
-response = requests.post(token_url, body)
+response = requests.post(token_url, json=body)
 st.write(response.content)
 
 
