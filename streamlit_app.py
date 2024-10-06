cd2925c139b9bf7c4f2274af94a4ebf403b4909d
Dheeraj Ravindra Hegde <90691324+dheerajrhegde@users.noreply.github.com>
2024-10-06 17:58:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,8 +91,8 @@ else:
     url = "https://fhir.cigna.com/PatientAccess/v1-devportal/$userinfo"
     jsonString = requests.get(url, headers=headers)
     data = json.loads(jsonString.content)
-    st.write(token)
-    st.write(data)
+    #st.write(token)
+    #st.write(data)
     user_id = data["parameter"][0]["valueString"]
 
     # Get user details
@@ -102,9 +102,9 @@ else:
     url = f"https://fhir.cigna.com/PatientAccess/v1-devportal/Patient?_id={user_id}"
     #st.write(url)
     jsonString = requests.get(url, headers=headers)
-    st.write(jsonString)
+    #st.write(jsonString)
     data = json.loads(jsonString.content)
-    st.write(data)
+    #st.write(data)
     customer_name = data["entry"][0]["resource"]["name"][0]["given"][0]
     identifier = data["entry"][0]["resource"]["id"]
 
